97f1ef1af446d349963630f2b60da8a54ec3a0af
DavidM <162993605+DEIV0510@users.noreply.github.com>
2024-11-02 19:05:46 -0500

    Add files via upload

--- login2.js
+++ login2.js
@@ -1,4 +1,3 @@
-
 const container = document.querySelector(".container");
 const btnSignIn = document.getElementById("btn-sign-in");
 const btnSignUp = document.getElementById("btn-sign-up");
@@ -6,66 +5,62 @@ const formSignIn = document.querySelector('.sign-in');
 const formSignUp = document.querySelector('.sign-up');
 const containerWelcome = document.querySelector('.container-welcome');
 
-
+// Alternar entre los formularios de inicio de sesión y registro
 btnSignIn.addEventListener("click", () => {
     container.classList.remove("toggle");
     containerWelcome.classList.add('hidden');
     formSignIn.classList.remove('hidden');
+    formSignUp.classList.add('hidden');
 });
 
 btnSignUp.addEventListener("click", () => {
     container.classList.add("toggle");
     containerWelcome.classList.add('hidden');
     formSignUp.classList.remove('hidden');
+    formSignIn.classList.add('hidden');
 });
 
-
+// Función para el registro de usuario
 function signUp(event) {
     event.preventDefault();
     const nombre = document.querySelector('.sign-up input[name="nombre"]').value;
     const email = document.querySelector('.sign-up input[name="email"]').value;
     const password = document.querySelector('.sign-up input[name="password"]').value;
 
+    if (nombre && email && password) {
+        const user = { nombre, email, password };
+        const users = JSON.parse(localStorage.getItem('users')) || [];
+        users.push(user);
+        localStorage.setItem('users', JSON.stringify(users));
 
-    const user = {
-        nombre,
-        email,
-        password,
-    };
-
-
-    const users = JSON.parse(localStorage.getItem('users')) || [];
-    users.push(user);
-    localStorage.setItem('users', JSON.stringify(users));
-
-
-    alert('Registro exitoso!');
-    formSignUp.reset(); 
+        alert('Registro exitoso!');
+        formSignUp.reset(); 
+        container.classList.remove("toggle");
+        formSignUp.classList.add('hidden');
+        formSignIn.classList.remove('hidden');
+    } else {
+        alert('Por favor, complete todos los campos');
+    }
 }
 
-
+// Función para iniciar sesión de usuario
 function signIn(event) {
     event.preventDefault();
     const email = document.querySelector('.sign-in input[name="email"]').value;
     const password = document.querySelector('.sign-in input[name="password"]').value;
 
-
     const users = JSON.parse(localStorage.getItem('users')) || [];
-
-
     const user = users.find((user) => user.email === email && user.password === password);
 
     if (user) {
-
         alert('Inicio de sesión exitoso!');
-       
-        window.location.href = 'PAGINA/index.html'; 
+        localStorage.setItem('loggedInUserEmail', email);  // Guardar el email del usuario logueado
+        window.location.href = '../PAGINA/index.html';  // Redirigir a la página de cuenta
     } else {
-        
         alert('Correo electrónico o contraseña incorrectos');
     }
 }
 
-
+// Event listeners para enviar los formularios
 formSignUp.addEventListener('submit', signUp);
 formSignIn.addEventListener('submit', signIn);
